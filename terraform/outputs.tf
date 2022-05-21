8b56a5a72b3dc2a4fc73e388a5cc305d8fc03189
ricardo-f <riclima1990@gmail.com>
2022-05-21 12:53:41 -0300

    testing repo removal

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
-output "ssh_clone_url" {
-  value       = github_repository.example.ssh_clone_url
-  description = "URL git clone the repository via SSH."
-}
+# output "ssh_clone_url" {
+#   value       = github_repository.example.ssh_clone_url
+#   description = "URL git clone the repository via SSH."
+# }
 
-output "http_clone_url" {
-  value       = github_repository.example.http_clone_url
-  description = "URL git clone the repository via HTTPS."
-}
+# output "http_clone_url" {
+#   value       = github_repository.example.http_clone_url
+#   description = "URL git clone the repository via HTTPS."
+# }
