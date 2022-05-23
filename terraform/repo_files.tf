7ca8139e983e169054f66dc43a63705205cb8196
ricardo-f <riclima1990@gmail.com>
2022-05-23 18:25:28 -0300

    adjust name reference and add meta-arg life cicle to avoid changes after creation

--- terraform/repo_files.tf
+++ terraform/repo_files.tf
@@ -1,10 +1,14 @@
 resource "github_repository_file" "gitignore_file" {
   for_each = github_repository.public
 
-  repository          = each.value.node_id
+  repository          = each.value.name
   branch              = "main"
   file                = ".gitignore"
   content             = file("template_files/.gitignore")
   commit_message      = "Managed by GMaC"
   overwrite_on_create = false
+
+  lifecycle {
+    ignore_changes = [ content, sha ]
+  }
 }
