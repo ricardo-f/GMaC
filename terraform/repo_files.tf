9e59ac87a769079d83ffdb2de41b0e2ee57c4592
ricardo-f <riclima1990@gmail.com>
2022-05-27 13:48:20 -0300

    adding license templates for repos

--- terraform/repo_files.tf
+++ terraform/repo_files.tf
@@ -12,3 +12,18 @@ resource "github_repository_file" "gitignore_file" {
     ignore_changes = [content]
   }
 }
+
+resource "github_repository_file" "license_file" {
+  for_each = github_repository.public
+
+  repository          = each.value.name
+  branch              = "main"
+  file                = "LICENSE"
+  content             = file("template_files/LICENSE")
+  commit_message      = "Managed by GMaC"
+  overwrite_on_create = false
+
+  lifecycle {
+    ignore_changes = [content]
+  }
+}
